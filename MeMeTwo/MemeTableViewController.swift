3193f987eb24b6ae0646e0855773b2ef7946be73
Fatima Aroshod <alroshodfatima@gmail.com>
2019-10-16 00:16:10 +0300

    feat: added delete feature to the meme app.

--- MeMeTwo/MemeTableViewController.swift
+++ MeMeTwo/MemeTableViewController.swift
@@ -50,6 +50,39 @@ class MemeTableViewController: UITableViewController {
                navigationController!.pushViewController(detailController, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // Getting the meme title to present it to user before deletion
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        // declare delete action
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { (contextualAction, view, actionPerformed: @escaping (Bool) -> Void) in
+            
+            let alert = UIAlertController(title: "Delete Meme", message: "Are you sure you want to delete Meme: \(meme.topText ?? "") ... \(meme.bottomText ?? "") ?", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (alertAction) in
+                actionPerformed(false)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (alertAction) in
+                // perform delete
+                self.deleteMeme(index: indexPath.row)
+                //tableView.reloadData()
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                actionPerformed(true)// to indecate that the action successed and remove the action with the row
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        // Adding the action to the configuration and return it
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
+    
+    func deleteMeme(index: Int){
+        // Delete meme from the memes array in the Application Delegate
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        appDelegate.memes.remove(at: index)
+    }
+    
     // MARK: Add button action
     @IBAction func addMemeButton(){
         
